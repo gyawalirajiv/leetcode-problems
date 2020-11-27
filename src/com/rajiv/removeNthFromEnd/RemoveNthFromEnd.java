b09d5aa240836da2239234c67edcf4899a38be76
Rajiv Gyawali <gyawalirajiv@gmail.com>
2020-11-27 16:00:55 +0545

    RemoveNthFromEnd Added

--- src/com/rajiv/removeNthFromEnd/RemoveNthFromEnd.java
+++ src/com/rajiv/removeNthFromEnd/RemoveNthFromEnd.java
@@ -1,19 +1,19 @@
 package com.rajiv.removeNthFromEnd;
 import com.company.helperClasses.ListNode;
 
+//https://leetcode.com/problems/remove-nth-node-from-end-of-list/
 public class RemoveNthFromEnd {
     public static void main(String[] args) {
-        ListNode node = new ListNode(new int[]{1, 2});
+        ListNode node = new ListNode(new int[]{1, 2, 3, 4, 5});
         System.out.println(removeNthFromEnd(node, 2));
     }
 
-    public static ListNode removeNthFromEnd(ListNode head, int n) {
+    public static ListNode removeNthFromEnd(ListNode head, int n){
         if(head == null) return null;
         if(head.next == null && n == 1) return null;
 
         ListNode fast = head;
         ListNode slow = head;
-
         while (n != 0){
             fast = fast.next;
             n--;
